d015c28070548a07f6eb761ea408caf68630b3f3
Hoi Ki Mak <>
2021-03-18 16:46:56 +0800

    correct mysql account

--- 3100_backend/knexfile.js
+++ 3100_backend/knexfile.js
@@ -1,13 +1,13 @@
 module.exports = {
     development: {
       client: "mysql",
-      version: "8.0.12",
+      version: "8.0.22",
       connection: {
         host: "127.0.0.1",
         port: 3306,
-        user: "jojo",
-        password: 'jojoaigniter',
-        database: "3100proj",
+        user: "root",
+        password: '08ce267f',
+        database: "3100_project"
       },
       debug: true,
       migrations: {
