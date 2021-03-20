b26c6dbb4611d4bbc2cd03f76154ba0448b0f230
carmen473 <1155109846@link.cuhk.edu.hk>
2021-03-20 16:55:05 +0800

    commit on carmen

--- 3100_backend/knexfile.js
+++ 3100_backend/knexfile.js
@@ -6,8 +6,8 @@ module.exports = {
     connection: {
       host: "127.0.0.1",
       port: 3306,
-      user: "jojo",
-      password: 'jojoaigniter',
+      user: "OMG",
+      password: '1234567',
       database: "3100proj",
     },
     debug: true,
