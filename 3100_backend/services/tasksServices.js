662f7976d2fe93aaebb55031a90d5c81db3cd9e1
Hoi Ki Mak <>
2021-04-02 00:16:52 +0800

    merge with carmen

--- 3100_backend/services/tasksServices.js
+++ 3100_backend/services/tasksServices.js
@@ -23,12 +23,15 @@ module.exports = {
         });
     },
 
-    countTask: async function(user){
+    countTask: async function(user, start, end){
         return await knex('task')
             .count('*', {as: 'number'})
             .innerJoin('group','group.task_id','task.task_id')
             .where ({user_id: user})
-            .whereNotNull('completed_timestamp');
+            .whereNotNull('completed_timestamp')
+            .where('completed_timestamp', '>=', start)
+            .where('completed_timestamp', '<', end+1);
+            // .where('completed_timestamp', '<', '2021-03-16T23:59:59Z');
     },
 
     createTask: async function(newTask){
@@ -41,12 +44,14 @@ module.exports = {
             description: newTask.description
         });
     },
-    countTask2: async function(user){
+    countTask2: async function(user, start, end){
         return await knex('task')
             .count('*', {as: 'number'})
             .innerJoin('group','group.task_id','task.task_id')
             .where ({user_id: user})
-            .whereNull('completed_timestamp');
+            .whereNull('completed_timestamp')
+            .where('due_date', '>=', start)
+            .where('due_date', '<', end+1);
     },
     createSubTask: async function(newSubTask){
         console.log("create subtask service");
@@ -65,6 +70,8 @@ module.exports = {
             .where({task_id: task})
             .where({user_id: user})
             .update({subtask_id: subid});
+            
     }
+
     
 }
